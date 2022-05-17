4944adc03ba6ae1c13d1144ac3bc974c1ddd85c9
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-05-16 17:38:04 -0700

    Delete Footer.js

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <p><small>Jide Abatti &copy; 2022</small></p>
-        </footer>
-    );
-}
-
-export default Footer;

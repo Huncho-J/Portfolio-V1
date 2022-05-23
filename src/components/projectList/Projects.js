476835888102e1c4dc9df7cf605a1d633ff2dbdb
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-05-22 20:23:54 -0700

    Update Projects.js

--- src/components/projectList/Projects.js
+++ src/components/projectList/Projects.js
@@ -9,18 +9,19 @@ const Projects = () => {
     return (
        
             <section id='projects'>
-                <h5>Some of my Recent Work</h5>
+                <h5>Some of my Recent Works</h5>
                 <h2>Portfolio Projects</h2>
                 <div className="container works-container">
                 {
-                    projects.map(({id, image, title, link}) => {
+                    projects.map(({id, image, title, link,code, description}) => {
                         return(
                             <article key={id} className='works-item'>
                                 <div className='works-item-img'>
                                  <img src={image} alt={title}/>
                                      <h3>{title}</h3>
+                                     <h5>{description}</h5>
                                <div className='btn-cta'>
-                                    <a href={link} className='btn'>Github Code</a>
+                                    <a href={code} className='btn btn-primary' target='blank'>Github Code</a>
                                     <a href={link} className='btn btn-primary' target='blank'>Live Demo</a>
                                   </div>
                             </div>

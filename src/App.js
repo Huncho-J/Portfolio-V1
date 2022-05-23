fefa1046019f6095a49ef234603e9d370a61c340
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-05-22 20:24:47 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // Import Components
+import Navbar from './components/navbar/Navbar';
 import Header from './components/Header/Header';
 import About from './components/About';
 import Services from './components/services/Services';
@@ -7,9 +8,15 @@ import Projects from './components/projectList/Projects';
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer';
 
+
 function App() {
+
+	
 	return (
+		
 		<div className="App">
+			
+			<Navbar></Navbar>
 			<Header />
 			<About />
 			<Experience />

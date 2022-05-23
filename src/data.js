20e70f1d2aeb5451291c2e92ef00ea0f3a4df05c
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-05-22 20:23:45 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,14 +1,31 @@
 export const projects = [
     {
       id: 1,
-      image:"https://i.ibb.co/SBdxMCs/web3flow.png",
+      image:"https://i.ibb.co/MGd1Bjw/projects.jpg" ,
       title:"Web3Flow",
-      link: "https://www.youtube.com/watch?v=4KxHcbQ8GYQ",
+      link: "https://huncho-j.github.io/Web3Flow/",
+      code:"https://github.com/Huncho-J/Web3Flow",
+      description: "web3flow is personal token bridge project. To learn more view the projects readme by clicking the github link below.",
     },
     {
       id: 2,
-      image: "",
-      title:"",
+      image: "https://i.ibb.co/MGd1Bjw/projects.jpg"  ,
+      title:"Portfolio Site V1",
       link: "http://github.io/huncho-j",
+      code:"https://github.com/Huncho-J/Portfolio-V1",
+      description: "You're currently on the first Iteration of my personal website. A fun project in which I learnt quite a bit about react, html, css & javascript.",
+    },
+    {
+      id: 3,
+      image: "https://i.ibb.co/MGd1Bjw/projects.jpg" ,
+      title:"Chainscope",
+      link: "https://github.com/Huncho-J/Chainscope/blob/main/README.md",
+      code:"https://github.com/Huncho-J/Chainscope",
+      description:"Chainscope is a smart contract powered supplychain project that aims to make the supplier and manufacturer relationship a trustless.",
     }
+
   ];
+
+
+
+

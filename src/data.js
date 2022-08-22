818d20536fc59615d90d3739f2208385c86b2843
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-08-22 15:38:39 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,14 +1,6 @@
 export const projects = [
     {
       id: 1,
-      image:"https://i.ibb.co/DCMZNnX/web3.png" ,
-      title:"Web3Flow",
-      link: "https://huncho-j.github.io/Web3Flow/",
-      code:"https://github.com/Huncho-J/Web3Flow",
-      description: "web3flow is personal token bridge project. To learn more view the projects readme by clicking the github link below.",
-    },
-    {
-      id: 2,
       image: "https://i.ibb.co/R9L7r6b/portfolio-V1.png"  ,
       title:"Portfolio Site V1",
       link: "https://jideportfolio.space",
@@ -16,12 +8,20 @@ export const projects = [
       description: "You're currently on the first Iteration of my personal website. A fun project in which I learnt quite a bit about react, html, css & javascript.",
     },
     {
-      id: 3,
+      id: 2,
       image: "https://i.ibb.co/rspV6HJ/sct.png" ,
       title:"SwapCentral v3",
       link: "https://swapcentral-v3.vercel.app/",
       code:"https://github.com/Huncho-J/SCT-v3/blob/main/README.md",
-      description:"Chainscope is a smart contract powered supplychain project that aims to make the supplier and manufacturer relationship a trustless.",
+      description:"SwapCentral is a prototype exchange platform powered by smart contracts.",
+    },
+    {
+      id: 3,
+      image:"https://i.ibb.co/DCMZNnX/web3.png" ,
+      title:"Web3Flow",
+      link: "https://huncho-j.github.io/Web3Flow/",
+      code:"https://github.com/Huncho-J/Web3Flow",
+      description: "web3flow is personal token bridge project. To learn more view the projects readme by clicking the github code link below.",
     }
 
   ];

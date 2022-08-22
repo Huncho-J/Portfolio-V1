f640833467356d0e3802585f30fb9a6abe2f9bd0
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-08-22 15:38:42 -0700

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,16 +7,16 @@ const Footer = () => {
             <a href='#' className='footer-logo'>Jide</a>
             <ul className='permalinks'>
                 <li><a href='#'>Home</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Expertise</a></li>
-                <li><a href='#'>Projects</a></li>
-                <li><a href='#'>Contact</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#expertise'>Expertise</a></li>
+                <li><a href='#projects'>Projects</a></li>
+                <li><a href='#contact'>Contact</a></li>
             </ul>
             <div className='footer-socials'>
-                <a href=''><AiFillGithub></AiFillGithub></a>
-                <a href=''><AiFillLinkedin></AiFillLinkedin></a>
-                <a href=''><AiFillThunderbolt></AiFillThunderbolt></a>
-                <a href=''><AiFillTwitterCircle></AiFillTwitterCircle></a>
+                <a href='https://github.com/Huncho-J'><AiFillGithub></AiFillGithub></a>
+                <a href='https://www.linkedin.com/in/olajide-abatti-jr-410361126'><AiFillLinkedin></AiFillLinkedin></a>
+                <a href='https://hire.scoutible.com/profile/share/ag9zfnNjb3V0aWJsZS1hcHByEQsSBFVzZXIYgICg0O-ViwoM'><AiFillThunderbolt></AiFillThunderbolt></a>
+                <a href='https://twitter.com/blair_baba'><AiFillTwitterCircle></AiFillTwitterCircle></a>
             </div>
             <small className='footer-cr'>Jide Abatti &copy; 2022</small>
         </footer>
